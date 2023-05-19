2a10f31ec6ac97be3d23d8be67c6e62ed8449212
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-19 00:02:39 -0400

    fixed Api and geocode issue, gto five day and current functions started

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var userFormEl = document.querySelector('#city-form');
 var nameInputEl = document.querySelector('#cityname');
 var WeatherEl = document.querySelector('#weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
-
+var key = '26cec7eda3c26c23cb50e92611870d4e'
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
   var city = nameInputEl.value.trim();
 
   if (city) {
-    getCityWeather(city);
-
+    // getCityWeather(city);
+    geoCode(city)
     WeatherEl.textContent = '';
     nameInputEl.value = '';
   } else {
@@ -19,18 +19,43 @@ var formSubmitHandler = function (event) {
   }
 };
 
-
-// var key = '301252a2716c4ab44bf8bb0c50a5efeb'
-
-var getCityWeather = function (city) {
-    var key = '301252a2716c4ab44bf8bb0c50a5efeb'
-  var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + {nameInputEl} + "&appid=" + {key};
-
-  fetch(apiUrl)
+function geoCode(city) {
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          displayRepos(data, city);
+          console.log(data);
+          getCityWeather(data[0].lat, data[0].lon)
+          getCityforecast(data[0].lat, data[0].lon)
+
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to weather');
+    });
+}
+// var key = '26cec7eda3c26c23cb50e92611870d4e'
+
+var getCityWeather = function (lat, lon) {
+  var key = '26cec7eda3c26c23cb50e92611870d4e'
+  var apiLatLon = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
+
+  fetch(apiLatLon)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          // displayRepos(data, city);
+          console.log(data);
+          var temp = document.createElement('h3')
+          temp.textContent = 'temp: ' + data.main.temp,
+
+
+
+
+            document.getElementById('forecast-container').append(temp)
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -41,15 +66,52 @@ var getCityWeather = function (city) {
     });
 };
 
+
+
+var getCityforecast = function (lat, lon) {
+  var key = '26cec7eda3c26c23cb50e92611870d4e'
+  var apiLatLon = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
+
+  fetch(apiLatLon)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          // displayRepos(data, city);
+          console.log(data);
+
+          for (let i = 3; i < data.list.length; i=i+8) {
+            console.log(data.list[i]);
+            var temp = document.createElement('h3')
+            temp.textContent = 'temp: ' + data.list[i].main.temp
+
+
+
+            document.getElementById('5dayWeather').append(temp)
+          }
+
+
+
+
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to weather');
+    });
+};
+
+
 function renderLastCity() {
-    var city = localStorage.getItem("city");
-  
-    if (!email) {
-      return;
-    }
-  
-    weatherSearchTerm.textContent = city;
+  var city = localStorage.getItem("city");
+
+  if (!city) {
+    return;
   }
+
+  weatherSearchTerm.textContent = city;
+}
 
 var displayweather = function (weather, searchTerm) {
   if (weather.length === 0) {
@@ -87,17 +149,15 @@ var displayweather = function (weather, searchTerm) {
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
-userFormEl.addEventListener('submit', function(event){
-    event.preventDefault
+userFormEl.addEventListener('submit', function (event) {
+  event.preventDefault
 
-    var city = document.querySelector("#cityname").valu;
+  var city = document.querySelector("#cityname").value;
 
-    if (city === ''){
-        alert("error")
-    } else {
-        alert("success")
-    }
+  // if (city != '') {
+  //   alert("error")
+  // } 
 
-    localStorage.setItem("city", city)
-    renderLastCity();
+  localStorage.setItem("city", city)
+  renderLastCity();
 });
