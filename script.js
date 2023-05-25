7f852ce36a87d834eee5b7389a5fd116ad349b32
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-25 18:48:54 -0400

    figured out other paramas for weather

--- script.js
+++ script.js
@@ -50,12 +50,24 @@ var getCityWeather = function (lat, lon) {
           // displayRepos(data, city);
           console.log(data);
           var temp = document.createElement('h3')
-          temp.textContent = 'temp: ' + data.main.temp,
+          temp.textContent = 'temp: ' + data.main.temp;
+
+          var humidity = document.createElement('h3')
+          humidity.textContent = 'humidity: ' + data.main.humidity;
+
+          var condition = document.createElement('h3')
+          condition.textContent = 'condition: '+ data.weather.main
+
+          var wind = document.createElement('h3')
+          wind.textContent = 'wind speed in Mi/Hr: '+ data.wind.speed
 
 
 
 
-            document.getElementById('forecast-container').append(temp)
+          document.getElementById('forecast-container').append(temp)
+          document.getElementById('forecast-container').append(humidity)
+          document.getElementById('forecast-container').append(condition)
+          document.getElementById('forecast-container').append(wind)
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -70,7 +82,7 @@ var getCityWeather = function (lat, lon) {
 
 var getCityforecast = function (lat, lon) {
   var key = '26cec7eda3c26c23cb50e92611870d4e'
-  var apiLatLon = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
+  var apiLatLon = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
 
   fetch(apiLatLon)
     .then(function (response) {
@@ -79,14 +91,19 @@ var getCityforecast = function (lat, lon) {
           // displayRepos(data, city);
           console.log(data);
 
-          for (let i = 3; i < data.list.length; i=i+8) {
+          for (let i = 3; i < data.list.length; i = i + 8) {
             console.log(data.list[i]);
             var temp = document.createElement('h3')
             temp.textContent = 'temp: ' + data.list[i].main.temp
 
+            var humidity = document.createElement('h3')
+            humidity.textContent = 'humidity: ' + data.list[i].main.humidity;
+
+
 
 
             document.getElementById('5dayWeather').append(temp)
+            document.getElementById('5dayWeather').append(humidity)
           }
 
 
@@ -113,40 +130,6 @@ function renderLastCity() {
   weatherSearchTerm.textContent = city;
 }
 
-var displayweather = function (weather, searchTerm) {
-  if (weather.length === 0) {
-    repoContainerEl.textContent = 'No Weather found found.';
-    return;
-  }
-
-  weatherSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < weather.length; i++) {
-    var repoName = weather[i].owner.login + '/' + weather[i].name;
-
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
 
 userFormEl.addEventListener('submit', formSubmitHandler);
 userFormEl.addEventListener('submit', function (event) {
@@ -154,9 +137,6 @@ userFormEl.addEventListener('submit', function (event) {
 
   var city = document.querySelector("#cityname").value;
 
-  // if (city != '') {
-  //   alert("error")
-  // } 
 
   localStorage.setItem("city", city)
   renderLastCity();
